a99b6ca147cba0ca0c994f4f1fa8ed206e7de3eb
Matheus Kennedy <matheuskennedyt@gmail.com>
2024-02-01 19:08:01 -0300

    Mudanças na classe DAO.

---
+++ src/main/java/org/wepayu/domain/entities/DAO/DAO.java
@@ -0,0 +1,32 @@
+package org.wepayu.domain.entities.DAO;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
+public class DAO <E>{
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+    private Class<E> classe;
+
+    static {
+        try{
+            emf = Persistence.createEntityManagerFactory("wepayu");
+        }catch(Exception e){
+
+        }
+
+    }
+
+    public DAO(){
+
+    }
+    public DAO(Class<E> classe){
+        this.classe = classe;
+        em = emf.createEntityManager();
+    }
+    public EntityManager getEntityManager(){
+        return em;
+    }
+}
